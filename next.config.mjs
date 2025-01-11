16910f8102bb1e8e855ab58042c04ce0d492fe3b
jorgluiz <jorgeluizti@hotmail.com>
2025-01-11 10:09:56 -0300

    feat: update og:image and twitter:image meta tag with new image for link sharing preview

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['portfolio-projects-production.up.railway.app'], // Adicione seu domínio aqui
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
