ba25c0e90bc6899498fb697902dbc84c8132278a
jorgluiz <jorgeluizti@hotmail.com>
2025-05-24 18:53:42 -0300

    Initialize GA in useEffect to run only on client side

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,24 +1,27 @@
-// import { useRouter } from 'next/router'
-// import { type AppProps } from 'next/app'
+import { useEffect } from 'react';
+import Script from 'next/script';
 
 import { GlobalStyle } from '@/components/globalStyles';
 import { GlobalStateProvider } from "../context/GlobalStateContext";
 
+const GA_ID = 'G-JXELT2K8RB';
+
 const MyApp = ({ Component, pageProps }) => {
-  // const router = useRouter()
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { window.dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', GA_ID);
+  }, []);
 
-  // useEffect(() => {
-  //   // Redirecionamento da página inicial para /layout
-  //   void router.replace('/layout')
-  // }, [router])
-
-  // // Renderiza apenas um placeholder enquanto o redirecionamento está sendo processado
-  // if (router.pathname !== '/layout') {
-  //   return null
-  // }
   return (
     <GlobalStateProvider>
       <GlobalStyle />
+      {/* Script externo do GA */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
       <Component {...pageProps} />
     </GlobalStateProvider>
   );
--- src/pages/_documents.jsx
+++ src/pages/_documents.jsx
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-const GTM_ID = 'GTM-5JCSZJV6';
-
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -34,24 +32,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt">
         <Head>
-          {/* Google Tag Manager - Script no head */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-              `,
-            }}
-          />
-          <link rel="icon" href="/favicon.ico" />
-          <meta charSet="utf-8" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
