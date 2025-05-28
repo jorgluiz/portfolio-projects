78190fb8a16c34dcc8e1c976d41105ab6ebf192d
jorgluiz <jorgeluizti@hotmail.com>
2025-05-28 02:58:16 -0300

    ising part of the code for performance testing

--- src/pages/projetos/index.jsx
+++ src/pages/projetos/index.jsx
@@ -1,33 +1,33 @@
 import Head from "next/head";
 import Image from 'next/image';
 import Link from "next/link";
-// import { useGlobalState } from "../../context/GlobalStateContext";
+import { useGlobalState } from "../../context/GlobalStateContext";
 
 import { Paragraph } from "@/styles/globalStyles";
 
-import portfolioImg from "../../assets/images/portfolio.png";
-import stripelogo from "../../assets/images/stripeLogo.png";
-import carousel from "../../assets/images/carousel.png";
-import analyzeFeelings from "../../assets/images/analyze-feelings.png";
-import stream from "../../assets/images/stream.png";
-import youtube from "../../assets/images/youtube.png";
-import imageGeneration from "../../assets/images/imageGeneration.png";
-import catalogo from "../../assets/images/catalogo.png";
-import ytmp3 from "../../assets/images/ytmp3.png";
-import ytmp4 from "../../assets/images/ytmp4.png";
-import cursoOline from "../../assets/images/curso-online.png";
-import ytAnalyzeVideo from "../../assets/images/yt-analyze-video.png";
+// import portfolioImg from "../../assets/images/portfolio.png";
+// import stripelogo from "../../assets/images/stripeLogo.png";
+// import carousel from "../../assets/images/carousel.png";
+// import analyzeFeelings from "../../assets/images/analyze-feelings.png";
+// import stream from "../../assets/images/stream.png";
+// import youtube from "../../assets/images/youtube.png";
+// import imageGeneration from "../../assets/images/imageGeneration.png";
+// import catalogo from "../../assets/images/catalogo.png";
+// import ytmp3 from "../../assets/images/ytmp3.png";
+// import ytmp4 from "../../assets/images/ytmp4.png";
+// import cursoOline from "../../assets/images/curso-online.png";
+// import ytAnalyzeVideo from "../../assets/images/yt-analyze-video.png";
 
-import { portfolioImgBlur } from "@/utils/portfolio";
-import { stripeImgBlur } from "@/utils/stripe";
-import { carouseImgBlur } from "@/utils/carousel";
-import { analyzeImgBlur } from "@/utils/analyze";
-import { streamImgBlur } from "@/utils/stream";
-import { imageGenerationImgBlur } from "@/utils/imageGeneration";
-import { youtubeImgBlur } from "@/utils/youtube";
-import { catalogoImgBlur } from "@/utils/catalogo";
-import { ytmp3ImgBlur } from "@/utils/ytmp3";
-import { ytmp4ImgBlur } from "@/utils/ytmp4";
+// import { portfolioImgBlur } from "@/utils/portfolio";
+// import { stripeImgBlur } from "@/utils/stripe";
+// import { carouseImgBlur } from "@/utils/carousel";
+// import { analyzeImgBlur } from "@/utils/analyze";
+// import { streamImgBlur } from "@/utils/stream";
+// import { imageGenerationImgBlur } from "@/utils/imageGeneration";
+// import { youtubeImgBlur } from "@/utils/youtube";
+// import { catalogoImgBlur } from "@/utils/catalogo";
+// import { ytmp3ImgBlur } from "@/utils/ytmp3";
+// import { ytmp4ImgBlur } from "@/utils/ytmp4";
 // import { cursoOnlineImgBlur } from "@/utils/ytmp4";
 
 import { MainLayout, MainContent } from "@/components/layout/mainLayout";
@@ -49,7 +49,7 @@ const tagStyle = {
 };
 
 const Projetos = ({ projetos }) => {
-  // const { isLoaded, isHamburguerOpen, setIsHamburguerOpen } = useGlobalState();
+  const { isLoaded, isHamburguerOpen, setIsHamburguerOpen } = useGlobalState();
 
   // Função para fechar sidebar clicando fora
   const handleOverlayClick = () => setIsHamburguerOpen(false);
@@ -79,55 +79,55 @@ const Projetos = ({ projetos }) => {
         <meta property="twitter:image" content="https://portfolio-projects-production.up.railway.app/perfil.jpg" />
       </Head>
 
-      {/* {!isLoaded ? null : ( */}
-      <>
-        {/* {isHamburguerOpen && (
+      {!isLoaded ? null : (
+        <>
+          {isHamburguerOpen && (
             <Overlay onClick={handleOverlayClick}></Overlay>
-          )} */}
-        <MainLayout>
-          <SideBarLayout></SideBarLayout>
-          <HeaderLayout></HeaderLayout>
-          <MainContentLayout>
-            <MainContent>
-              <h1 style={{ color: "323f4b" }}>Oi! 👋</h1>
-              <Paragraph>Este portfólio representa a síntese da minha jornada em desenvolvimento web e backend, demonstrando minha experiência com ReactJS no frontend e Express no backend para a construção de aplicações modernas e eficientes.</Paragraph>
-              {/* <GridContainer> */}
-              {/* {projetos.map((projeto, i) => (
-                  <div className="container-item" key={i}>
-                    <Link href={projeto.link}>
-                      <Image
-                        src={projeto.image}
-                        alt={projeto.alt}
-                        placeholder="blur"
-                        blurDataURL={projeto.blur}
-                        draggable={false}
-                        className="img-projetos"
-                        loading="lazy"
-                        width={439}  // Defina uma largura base proporcional
-                        height={244}  // A altura é fixada como você já definiu no CSS
-                      />
-                      <div className="item">
-                        <div className="sub-item">
-                          <div className="tech">
-                            <h1 className="item-h1" style={{ display: "inline" }}>Tech:</h1>
-                            {projeto.tech.map((tech, idx) => (
-                              <span key={idx} style={tagStyle}>{tech}</span>
-                            ))}
+          )}
+          <MainLayout>
+            <SideBarLayout></SideBarLayout>
+            <HeaderLayout></HeaderLayout>
+            <MainContentLayout>
+              <MainContent>
+                <h1 style={{ color: "323f4b" }}>Oi! 👋</h1>
+                <Paragraph>Este portfólio representa a síntese da minha jornada em desenvolvimento web e backend, demonstrando minha experiência com ReactJS no frontend e Express no backend para a construção de aplicações modernas e eficientes.</Paragraph>
+                <GridContainer>
+                  {projetos.map((projeto, i) => (
+                    <div className="container-item" key={i}>
+                      <Link href={projeto.link}>
+                        <Image
+                          src={projeto.image}
+                          alt={projeto.alt}
+                          // placeholder="blur"
+                          // blurDataURL={projeto.blur}
+                          draggable={false}
+                          className="img-projetos"
+                          loading="lazy"
+                          width={439}  // Defina uma largura base proporcional
+                          height={244}  // A altura é fixada como você já definiu no CSS
+                        />
+                        <div className="item">
+                          <div className="sub-item">
+                            <div className="tech">
+                              <h1 className="item-h1" style={{ display: "inline" }}>Tech:</h1>
+                              {projeto.tech.map((tech, idx) => (
+                                <span key={idx} style={tagStyle}>{tech}</span>
+                              ))}
+                            </div>
+                            <h2 className="item-h2">{projeto.title}</h2>
+                            <p className="item-p">{projeto.descricao}</p>
+                            <p>{projeto.estrelas}</p>
                           </div>
-                          <h2 className="item-h2">{projeto.title}</h2>
-                          <p className="item-p">{projeto.descricao}</p>
-                          <p>{projeto.estrelas}</p>
                         </div>
-                      </div>
-                    </Link>
-                  </div>
-                ))} */}
-              {/* </GridContainer> */}
-            </MainContent>
-          </MainContentLayout>
-        </MainLayout>
-      </>
-      {/* )} */}
+                      </Link>
+                    </div>
+                  ))}
+                </GridContainer>
+              </MainContent>
+            </MainContentLayout>
+          </MainLayout>
+        </>
+      )}
     </>
   );
 };
@@ -142,7 +142,7 @@ export async function getStaticProps() {
           title: "Custom Video Player",
           link: "/projetos/player-video-customizado",
           image: "/assets/images/youtube.png",
-          blur: youtubeImgBlur,
+          // blur: youtubeImgBlur,
           tech: ["React.js"],
           descricao: "Estudos e aprendizado",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -152,7 +152,7 @@ export async function getStaticProps() {
           title: "Plataforma vendas cursos online",
           link: "/projetos/vendas-curso",
           image: "/assets/images/curso-online.png",
-          blur: ytmp4ImgBlur,
+          // blur: ytmp4ImgBlur,
           tech: [
             "React.js", "React Router DOM", "Context API", "Axios", "JWT-Decode",
             "Styled Components", "LocalStorage", "Node.js + Express", "PostgreSQL",
@@ -166,7 +166,7 @@ export async function getStaticProps() {
           title: "Data Analysis Tendency",
           link: "/projetos/data-analysis-twitter",
           image: "/assets/images/analyze-feelings.png",
-          blur: analyzeImgBlur,
+          // blur: analyzeImgBlur,
           tech: ["Node.js e Express", "Puppeteer", "Puppeteer-Extra", "OpenAI API"],
           descricao: "Estudos e aprendizado",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -176,7 +176,7 @@ export async function getStaticProps() {
           title: "Live Broadcast Control and Viewing System",
           link: "/projetos/stream-dolby",
           image: "/assets/images/stream.png",
-          blur: streamImgBlur,
+          // blur: streamImgBlur,
           tech: ["Node.js e Express", "Millicast API", "EJS", "HTML/CSS", "GraphQL", "dotenv", "axios e https"],
           descricao: "Sistema de controle e visualização de transmissão ao vivo.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -186,7 +186,7 @@ export async function getStaticProps() {
           title: "Stripe",
           link: "/projetos/stripe",
           image: "/assets/images/stripeLogo.png",
-          blur: stripeImgBlur,
+          // blur: stripeImgBlur,
           tech: ["Node.js e Express.js", "JWT", "Stripe", "Firebase", "Middleware e Cookie", "dotenv", "nodemon"],
           descricao: "Integração com Stripe para pagamentos.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -196,7 +196,7 @@ export async function getStaticProps() {
           title: "Carousel",
           link: "/projetos/carousel",
           image: "/assets/images/carousel.png",
-          blur: carouseImgBlur,
+          // blur: carouseImgBlur,
           tech: ["React", "Next.js", "styled-components"],
           descricao: "Carrossel interativo de imagens.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -206,7 +206,7 @@ export async function getStaticProps() {
           title: "Enlarged View Image Generator",
           link: "/projetos/image-generator",
           image: "/assets/images/imageGeneration.png",
-          blur: imageGenerationImgBlur,
+          // blur: imageGenerationImgBlur,
           tech: ["HTML5", "CSS3", "JavaScript", "Backend"],
           descricao: "Gerador de imagens ampliadas.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -216,7 +216,7 @@ export async function getStaticProps() {
           title: "Catalog",
           link: "/projetos/catalogo",
           image: "/assets/images/catalogo.png",
-          blur: catalogoImgBlur,
+          // blur: catalogoImgBlur,
           tech: ["React.js", "CSS"],
           descricao: "Catálogo de itens.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -226,7 +226,7 @@ export async function getStaticProps() {
           title: "Download de Vídeo do YouTube (Cloud)",
           link: "/projetos/yt-for-mp4-cloud",
           image: "/assets/images/ytmp4.png",
-          blur: ytmp4ImgBlur,
+          // blur: ytmp4ImgBlur,
           tech: ["HTML e CSS", "Node.js com Express.js", "youtube-dl-exec e @distube/ytdl-core", "fluent-ffmpeg", "Socket.io", "CORS"],
           descricao: "Download de vídeos do YouTube (Backend na nuvem).",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -236,7 +236,7 @@ export async function getStaticProps() {
           title: "Download de Vídeo do YouTube (Local)",
           link: "/projetos/yt-for-mp4-location",
           image: "/assets/images/ytmp4.png",
-          blur: ytmp4ImgBlur,
+          // blur: ytmp4ImgBlur,
           tech: ["HTML e CSS", "Socket.IO", "Node.js", "Express", "@distube/ytdl-core"],
           descricao: "Download de vídeos do YouTube (Backend local).",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -246,7 +246,7 @@ export async function getStaticProps() {
           title: "Download de Áudio do YouTube",
           link: "/projetos/yt-for-mp3",
           image: "/assets/images/ytmp3.png",
-          blur: ytmp3ImgBlur,
+          // blur: ytmp3ImgBlur,
           tech: ["HTML e CSS", "Node.js", "Express", "@distube/ytdl-core"],
           descricao: "Download de áudio do YouTube.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -256,7 +256,7 @@ export async function getStaticProps() {
           title: "YouTube Video Analyze",
           link: "/projetos/yt-analyse-video",
           image: "/assets/images/yt-analyze-video.png",
-          blur: ytmp3ImgBlur,
+          // blur: ytmp3ImgBlur,
           tech: ["Node.js e Express.js", "OpenAI API", "File System (fs)", "dotenv", "youtubedl-exec", "hbs (Handlebars)"],
           descricao: "Análise de vídeos do YouTube com OpenAI.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
@@ -266,7 +266,7 @@ export async function getStaticProps() {
           title: "Portfolio",
           link: "/projetos/portfolio",
           image: "/assets/images/portfolio.png",
-          blur: portfolioImgBlur,
+          // blur: portfolioImgBlur,
           tech: ["React", "Styled-components"],
           descricao: "Portfólio de projetos web.",
           estrelas: "⭐⭐⭐⭐⭐ 🚀",
