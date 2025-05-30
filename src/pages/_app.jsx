d355006e097644f5e540845acece983ba7ce8119
jorgluiz <jorgeluizti@hotmail.com>
2025-05-30 20:38:55 -0300

    refactor: improve Google Analytics integration in Next.js

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -29,7 +29,51 @@
 
 // export default MyApp;
 
-import { useEffect } from 'react';
+// -----------------------------------------------------------------
+
+// import { useEffect } from 'react';
+// import Script from 'next/script';
+
+// import { GlobalStyle } from '@/styles/globalStyles';
+// import { GlobalStateProvider } from '../context/GlobalStateContext';
+
+// const GA_ID = 'G-T9MLFWWB7M';
+
+// const MyApp = ({ Component, pageProps }) => {
+
+//   const ENABLE_GA = process.env.NEXT_PUBLIC_ENABLE_GA === 'true';
+//   console.log(process.env.NEXT_PUBLIC_ENABLE_GA, "process.env.NEXT_PUBLIC_ENABLE_GA");
+//   console.log(ENABLE_GA, "ENABLE_GA");
+
+//   useEffect(() => {
+//     if (!ENABLE_GA) return;
+
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag() {
+//       window.dataLayer.push(arguments);
+//     }
+//     gtag('js', new Date());
+//     gtag('config', GA_ID);
+//   }, []);
+
+//   return (
+//     <GlobalStateProvider>
+//       <GlobalStyle />
+//       {ENABLE_GA && (
+//         <Script
+//           strategy="afterInteractive"
+//           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+//         />
+//       )}
+//       <Component {...pageProps} />
+//     </GlobalStateProvider>
+//   );
+// };
+
+// export default MyApp;
+
+
+import { useState } from 'react';
 import Script from 'next/script';
 
 import { GlobalStyle } from '@/styles/globalStyles';
@@ -38,34 +82,35 @@ import { GlobalStateProvider } from '../context/GlobalStateContext';
 const GA_ID = 'G-T9MLFWWB7M';
 
 const MyApp = ({ Component, pageProps }) => {
-
   const ENABLE_GA = process.env.NEXT_PUBLIC_ENABLE_GA === 'true';
-  console.log(process.env.NEXT_PUBLIC_ENABLE_GA, "process.env.NEXT_PUBLIC_ENABLE_GA");
-  console.log(ENABLE_GA, "ENABLE_GA");
+  const [gaInitialized, setGaInitialized] = useState(false);
 
-  useEffect(() => {
-    if (!ENABLE_GA) return;
-
+  const handleGtagLoad = () => {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       window.dataLayer.push(arguments);
     }
     gtag('js', new Date());
     gtag('config', GA_ID);
-  }, []);
+    setGaInitialized(true);
+  };
 
   return (
     <GlobalStateProvider>
       <GlobalStyle />
+
       {ENABLE_GA && (
         <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          onLoad={handleGtagLoad}
         />
       )}
+
       <Component {...pageProps} />
     </GlobalStateProvider>
   );
 };
 
 export default MyApp;
+
