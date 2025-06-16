417ed0357e8b905eab1984645b29f2c3f07aa057
jorgluiz <jorgeluizti@hotmail.com>
2025-06-16 11:08:47 -0300

    refactor(ui): Replace global highlight.js with CodeBlock component in utm-parameters

--- src/pages/marketing-digital/utm-parameters/index.jsx
+++ src/pages/marketing-digital/utm-parameters/index.jsx
@@ -4,11 +4,8 @@ import { useGlobalState } from "../../../context/GlobalStateContext";
 import {
   Container,
   SectionTitle,
-  Subtitle,
   Paragraph,
-  CodeBlock,
   List,
-  Link,
   CodeContainer
 } from "@/styles/globalStyles";
 
@@ -17,21 +14,13 @@ import SideBarLayout from "@/components/layout/sideBarLayout";
 import HeaderLayout from "@/components/layout/headerLayout";
 import MainContentLayout from "@/components/layout/contentLayout";
 import Overlay from "@/components/overlay";
-
-import hljs from 'highlight.js'; // Importando o highlight.js
-import 'highlight.js/styles/atom-one-dark.css'; // Estilo para colorir a sintaxe
+import CodeBlock from '@/components/codeBlock';
 
 const GA4NextJsIntegration = () => {
   const { isLoaded, isHamburguerOpen, setIsHamburguerOpen } = useGlobalState();
 
   const handleOverlayClick = () => setIsHamburguerOpen(false);
 
-  useEffect(() => {
-    // Só executa a estilização se o conteúdo estiver carregado e visível
-    if (isLoaded) {
-      hljs.highlightAll();
-    }
-  }, [isLoaded]); // Adicione 'isLoaded' como dependência!
   return (
     <>
       <Head>
@@ -109,11 +98,10 @@ const GA4NextJsIntegration = () => {
                   </Paragraph>
                   <Paragraph>
                     Você pode criar um link para o WhatsApp assim:
-                    <pre>
-                      <code>
-                        {`https://portfolio-projects-production.up.railway.app?utm_source=whatsapp&utm_medium=social&utm_campaign=compartilhamento`}
-                      </code>
-                    </pre>
+                    <CodeBlock language={"javascript"}>
+
+                    </CodeBlock>
+                    {`https://portfolio-projects-production.up.railway.app?utm_source=whatsapp&utm_medium=social&utm_campaign=compartilhamento`}
                   </Paragraph>
 
                   <SectionTitle>Como visualizar no Google Analytics:</SectionTitle>
@@ -132,11 +120,9 @@ const GA4NextJsIntegration = () => {
                   <Paragraph>
                     Se você criar esse link para compartilhar no WhatsApp:
                   </Paragraph>
-                  <pre>
-                    <code>
-                      {`https://portfolio-projects-production.up.railway.app?utm_source=whatsapp&utm_medium=social&utm_campaign=compartilhamento`}
-                    </code>
-                  </pre>
+                  <CodeBlock language={"javascript"}>
+                    {`https://portfolio-projects-production.up.railway.app?utm_source=whatsapp&utm_medium=social&utm_campaign=compartilhamento`}
+                  </CodeBlock>
                   <Paragraph>
                     E alguém clicar nele, o Google Analytics registrará que o tráfego veio do WhatsApp <strong>(utm_source)</strong>, por um link social <strong>(utm_medium)</strong> e foi parte da campanha de compartilhamento <strong>(utm_campaign)</strong>.
                   </Paragraph>
@@ -153,56 +139,39 @@ const GA4NextJsIntegration = () => {
                   <Paragraph><strong>1. Campanha de Compartilhamento em WhatsApp</strong></Paragraph>
                   <Paragraph><strong>Mídia</strong>: social</Paragraph>
                   <Paragraph>Parâmetros UTM:</Paragraph>
-                  <pre>
-                    <code className='javascript'>
-                      {`utm_source=whatsapp&utm_medium=social&utm_campaign=compartilhamento`}
-                    </code>
-                  </pre>
-
+                  <CodeBlock language={"javascript"}>
+                    {`utm_source=whatsapp&utm_medium=social&utm_campaign=compartilhamento`}
+                  </CodeBlock>
                   <Paragraph><strong>2. Campanha de E-mail Marketing</strong></Paragraph>
                   <Paragraph><strong>Mídia</strong>: email</Paragraph>
                   <Paragraph>Parâmetros UTM:</Paragraph>
-                  <pre>
-                    <code className='javascript'>
-                      {`utm_source=email&utm_medium=email&utm_campaign=oferta_verao`}
-                    </code>
-                  </pre>
-
+                  <CodeBlock language={"javascript"}>
+                    {`utm_source=email&utm_medium=email&utm_campaign=oferta_verao`}
+                  </CodeBlock>
                   <Paragraph><strong>3. Campanha de Google Ads (CPC)</strong></Paragraph>
                   <Paragraph><strong>Mídia</strong>: cpc</Paragraph>
                   <Paragraph>Parâmetros UTM:</Paragraph>
-                  <pre>
-                    <code className='javascript'>
-                      {`utm_source=google&utm_medium=cpc&utm_campaign=promo_julho`}
-                    </code>
-                  </pre>
-
+                  <CodeBlock language={"javascript"}>
+                    {`utm_source=google&utm_medium=cpc&utm_campaign=promo_julho`}
+                  </CodeBlock>
                   <Paragraph><strong>4. Campanha de Afiliados</strong></Paragraph>
                   <Paragraph><strong>Mídia</strong>: link</Paragraph>
                   <Paragraph>Parâmetros UTM:</Paragraph>
-                  <pre>
-                    <code className='javascript'>
-                      {`utm_source=afiliado&utm_medium=link&utm_campaign=campanha_afiliado`}
-                    </code>
-                  </pre>
-
+                  <CodeBlock language={"javascript"}>
+                    {`utm_source=afiliado&utm_medium=link&utm_campaign=campanha_afiliado`}
+                  </CodeBlock>
                   <Paragraph><strong>5. Campanha de Teste A/B (Anúncios)</strong></Paragraph>
                   <Paragraph><strong>Mídia</strong>: paid</Paragraph>
                   <Paragraph>Parâmetros UTM:</Paragraph>
-                  <pre>
-                    <code className='javascript'>
-                      {`utm_source=facebook&utm_medium=paid&utm_campaign=teste_ab&utm_content=anuncio_a`}
-                    </code>
-                  </pre>
-
+                  <CodeBlock language={"javascript"}>
+                    {`utm_source=facebook&utm_medium=paid&utm_campaign=teste_ab&utm_content=anuncio_a`}
+                  </CodeBlock>
                   <Paragraph><strong>6. Campanha de Link Compartilhado em Blog</strong></Paragraph>
                   <Paragraph><strong>Mídia</strong>: referral</Paragraph>
                   <Paragraph>Parâmetros UTM:</Paragraph>
-                  <pre>
-                    <code className='javascript'>
-                      {`utm_source=blog&utm_medium=referral&utm_campaign=parceiros`}
-                    </code>
-                  </pre>
+                  <CodeBlock language={"javascript"}>
+                    {`utm_source=blog&utm_medium=referral&utm_campaign=parceiros`}
+                  </CodeBlock>
                 </Container>
               </CodeContainer>
             </MainContentLayout>
