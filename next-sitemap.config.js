add4bfd0fc64744911a75c45ae1f912abf787584
jorgluiz <jorgeluizti@hotmail.com>
2026-05-01 17:52:14 -0300

    Generate new sitemap-0.xml with new routes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://portfolio-projects-production.up.railway.app', // Seu domínio
+  siteUrl: 'https://portfolio-projects-one-psi.vercel.app/', // Seu domínio
   generateRobotsTxt: true, // Se você quiser gerar o arquivo robots.txt
   sitemapSize: 5000, // Número máximo de URLs por sitemap
 };
